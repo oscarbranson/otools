f9c7b9bfa93464b45f8f2987eab1576d5e1ecb46
Oscar <oscarbranson@gmail.com>
2018-03-13 14:43:34 +1100

    Updated calc_M to use decompose_molecule sub-function. Improved seawater composition function Add an 'all_isotopes' option to elements

--- otools/chemistry.py
+++ otools/chemistry.py
@@ -7,7 +7,7 @@ import pickle
 import pandas as pd
 
 
-def elements():
+def elements(all_isotopes=True):
     """
     Loads a DataFrame of all elements and isotopes.
 
@@ -17,7 +17,15 @@ def elements():
     -------
     pandas DataFrame with columns (element, atomic_number, isotope, atomic_weight, percent)
     """
-    return pd.read_pickle(os.path.dirname(__file__) + '/periodic_table/elements.pkl')
+    el = pd.read_pickle(os.path.dirname(__file__) + '/periodic_table/elements.pkl')
+    if all_isotopes:
+        return el
+    else:
+        def wmean(g):
+            return (g.atomic_weight * g.percent).sum() / 100
+        iel = el.groupby('element').apply(wmean)
+        iel.name = 'atomic_weight'
+        return iel
 
 
 def periodic_table():
@@ -33,59 +41,118 @@ def periodic_table():
     with open(os.path.dirname(__file__) + '/periodic_table/periodic_table.pkl', 'rb') as f:
         return pickle.load(f)
 
+def decompose_molecule(molecule, n=1):
+    """
+    Returns the chemical constituents of the molecule, and their number.
+
+    Parameters
+    ----------
+    molecule : str
+        A molecule in standard chemical notation, 
+        e.g. 'CO2', 'HCO3' or 'B(OH)4'.
+    
+    Returns
+    -------
+    All elements in molecule with their associated counts : dict
+    """
+    if isinstance(n, str):
+        n = int(n)
+    
+    # define regexs
+    parens = re.compile('\(([A-z0-9()]+)\)([0-9]+)?')
+    stoich = re.compile('([A-Z][a-z]?)([0-9]+)?')
+
+    ps = parens.findall(molecule)  # find subgroups in parentheses
+    rem = parens.sub('', molecule)  # get remainder
+    
+    if len(ps) > 0:
+        for s, ns in ps:
+            comp = decompose_molecule(s, ns)
+        for k, v in comp.items():
+            comp[k] = v * n
+    else:
+        comp = {}
+        
+    for e, ns in stoich.findall(rem):
+        if e not in comp:
+            comp[e] = 0
+        if ns == '':
+            ns = 1 * n
+        else:
+            ns = int(ns) * n
+        comp[e] += ns
+
+    return comp
 
 def calc_M(molecule):
     """
     Returns molecular weight of molecule.
 
-    Where molecule is in standard chemical notation,
-    e.g. 'CO2', 'HCO3' or B(OH)4
+    Parameters
+    ----------
+    molecule : str
+        A molecule in standard chemical notation, 
+        e.g. 'CO2', 'HCO3' or 'B(OH)4'.
+    
+    Returns
+    -------
+    Molecular weight of molecule : dict
+    """
+    # load periodic table
+    els = elements(all_isotopes=False)
+
+    comp = decompose_molecule(molecule)
+
+    m = 0
+    for k, v in comp.items():
+        m += els[k] * v
+    
+    return m
+
+def seawater(Sal=35., unit='mol/kg'):
+    """
+    Standard mean composition of seawater.
+
+    From Dickson, Sabine and Christian (2007), Chapter 5, Table 3
+
+    @book{dickson2007guide,
+          title={Guide to best practices for ocean CO2 measurements.},
+          author={Dickson, Andrew Gilmore and Sabine, Christopher L and Christian, James Robert},
+          year={2007},
+          publisher={North Pacific Marine Science Organization},
+          howpublished="https://www.nodc.noaa.gov/ocads/oceans/Handbook_2007.html",
+          ISBN="1-897176-07-4"}
+        
+
+    Parameters
+    ----------
+    Sal : float
+        Salinity, default is 35
+    unit : str
+        Either 'mol/kg' or 'g/kg'.
 
     Returns
     -------
-    molecular_weight : float
+    Seawater composition in chosen units at specified salinity : dict
     """
+    sw = {"Cl": 0.54586,
+          "SO4": 0.02824,
+          "Br": 0.00084,
+          "F": 0.00007,
+          "Na": 0.46906,
+          "Mg": 0.05282,
+          "Ca": 0.01028,
+          "K": 0.01021,
+          "Sr": 0.00009,
+          "B": 0.00042}
 
-    # load periodic table
-    els = elements()
-    els.set_index('element', inplace=True)
-
-    # define regexs
-    parens = re.compile('\(([A-z0-9]+)\)([0-9]+)?')
-    stoich = re.compile('([A-Z][a-z]?)([0-9]+)?')
-
-    ps = parens.findall(molecule)  # find subgroups in parentheses
-    rem = parens.sub('', molecule)  # get remainder
-
-    m = 0
-    # deal with sub-groups
-    if len(ps) > 0:
-        for sub, ns in ps:
-            ms = 0
-            for e, n in stoich.findall(sub):
-                me = (els.loc[e, 'atomic_weight'] *
-                      els.loc[e, 'percent'] / 100).sum()
-                if n == '':
-                    n = 1
-                else:
-                    n = int(n)
-                ms += me * n
-            if ns == '':
-                ns = 1
-            else:
-                ns = int(ns)
-            m += ms * ns
-    # deal with remainder
-    for e, n in stoich.findall(rem):
-        me = (els.loc[e, 'atomic_weight'] *
-              els.loc[e, 'percent'] / 100).sum()
-        if n == '':
-            n = 1
-        else:
-            n = int(n)
-        m += me * n
-    return m
-
+    if Sal != 35:
+        for s in sw.keys():
+            sw[s] *= Sal / 35.
+    
+    if unit == 'g/kg':
+        for k, v in sw.items():
+            sw[k] = calc_M(k) * v
 
 if __name__ == '__main__':
     print()
